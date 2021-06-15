5934eb4148751af69542cd74c0f22807b630a55f
aaserra <agustinserra@live.com>
2021-06-15 20:07:15 -0300

    Removed commented code

--- src/main/java/examples/commandline/CommandLineApplication.java
+++ src/main/java/examples/commandline/CommandLineApplication.java
@@ -27,45 +27,6 @@ public class CommandLineApplication {
 		commandLinesBuilder.exec(new CommandVisitor());
 	}
 
-	/*
-	public static void main(String[] args) {
-
-		System.out.println("Command Line File Manager");
-		System.out.println("	MKDIR [name] - Makes a directory. Ex: MKDIR abc");
-		System.out.println("	TOUCH [name] - Creates a file. Ex: TOUCH file.txt");
-		System.out.println("	CD [name]    - Go to directory. Ex1: cd abc, Ex2: ROOT\\abc");
-		System.out.println("	PWD          - Shows the current directory.");
-		System.out.println("	LS           - List of files and directories inside the current directory.");
-		System.out.println("	QUIT         - Exit application.");
-
-		CommandLinesBuilder commandLinesBuilder = new CommandLinesBuilder();
-		boolean doExit = false;
-
-		do {
-			String[] commands = System.console().readLine("Write your commands: ").split("\\s");
-
-			for(int i = 0; i < commands.length ; i++) {
-				if (isCommand(commands[i])) {
-					CommandType cmdType = CommandType.getValue(commands[i]);
-					String cmdParam = null;
-
-					if (cmdType != null) {
-						if (cmdType == CommandType.QUIT) doExit = true;
-
-						if (i < commands.length - 1) cmdParam = isCommand(commands[i + 1]) ? null : commands[i + 1];
-
-						commandLinesBuilder.addCommand(cmdType, cmdParam);
-					}
-				}
-			}
-
-			commandLinesBuilder.exec(new CommandVisitor());
-
-		} while (!doExit);
-
-	}
-	*/
-
 	private static boolean isCommand(String text) {
 		return text.matches("[A-Z]+");
 	}
